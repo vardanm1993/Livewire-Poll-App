e492219a71a35c0c41ddb318aa67e3b2289053ff
vardanm1993 <vardanm1993@gmail.com>
2024-01-19 11:52:06 +0000

    Added polls listing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,14 @@
 </head>
 
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
-
-@livewire('create-poll')
+    <div>
+        <h2 class="mt-4 mb-4 text-2xl text-yellow-600">Create Poll</h2>
+        @livewire('create-poll')
+    </div>
+    <div class="mt-10">
+        <h2 class="mt-4 mb-4 text-2xl text-green-600">Available Polls</h2>
+        @livewire('polls')
+    </div>
 </body>
 
 </html>
