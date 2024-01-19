ca71f66ca5c8a8e2e710e89e2e3e79387b4d19ae
vardanm1993 <vardanm1993@gmail.com>
2024-01-19 11:52:47 +0000

    Added create poll event

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -37,12 +37,12 @@ class CreatePoll extends Component
         $this->options = array_values($this->options);
     }
 
-    public function updated($propertyName)
+    public function updated($propertyName): void
     {
         $this->validateOnly($propertyName);
     }
 
-    public function createPoll()
+    public function createPoll(): void
     {
         $this->validate();
 
@@ -55,5 +55,7 @@ class CreatePoll extends Component
         );
 
         $this->reset('title', 'options');
+
+        $this->dispatch('pollCreated');
     }
 }
