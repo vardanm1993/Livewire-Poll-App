2e54156d8f496560b6d1438edc02b1149e55ce02
vardanm1993 <vardanm1993@gmail.com>
2024-01-19 11:04:38 +0000

    Added errors display

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,8 +1,13 @@
 <div>
     <form wire:submit.prevent="createPoll">
         <label for="poll">Poll Title</label>
-        <input type="text" id="poll" wire:model="title">
+        <input type="text" id="poll" wire:model.live="title">
 
+        @error('title')
+            <div class="text-red-600">
+                {{ $message }}
+            </div>
+        @enderror
         <div class=" mt-4 mb-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
         </div>
@@ -12,11 +17,21 @@
                 <div class="mb-4">
                     <label for="option{{ $index + 1 }}">Option {{ $index + 1 }}</label>
                     <div class="flex gap-2">
-                        <input wire:model="options.{{ $index }}" id="option{{ $index + 1 }}" type="text">
+                        <input wire:model.live="options.{{ $index }}" id="option{{ $index + 1 }}" type="text">
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-600">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
             @endforeach
+                @error("options")
+                    <div class="text-red-600">
+                        {{ $message }}
+                    </div>
+                @enderror
         </div>
 
         <button type="submit" class="btn">Create Poll</button>
